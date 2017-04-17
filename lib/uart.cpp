69909aa232b3f521a46e8f48e91c349b366e5f38
Weidong Guo <weidongguo@live.com>
2017-04-16 23:31:03 -0700

    Added uart support

--- lib/uart.cpp
+++ lib/uart.cpp
@@ -1,29 +1,36 @@
-#include <stddef.h>
-#include <stdint.h>
 #include <hal/uart.h>
 #include <hal/bcm2835.h>
 #include <hal/bcm2836.h> 
-#include <hal/memio.h>
+//#include <hal/memio.h>
+#include <circle/types.h>
+#include <circle/memio.h>
+
+// Loop <delay> times in a way that the compiler won't optimize away
+static inline void delay(s32 count)
+{
+    asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n"
+         : "=r"(count): [count]"0"(count) : "cc");
+}
 
 void uart_init()
 {
 	// Disable UART0.ARM_UART0_CR
-	mmio_write(ARM_UART0_CR, 0x00000000);
+	write32(ARM_UART0_CR, 0x00000000);
 	// Setup the GPIO pin 14 && 15.
  
 	// Disable pull up/down for all GPIO pins & delay for 150 cycles.
-	mmio_write(ARM_GPIO_GPPUD, 0x00000000);
+	write32(ARM_GPIO_GPPUD, 0x00000000);
 	delay(150);
  
 	// Disable pull up/down for pin 14,15 & delay for 150 cycles.
-	mmio_write(ARM_GPIO_GPPUDCLK0, (1 << 14) | (1 << 15));
+	write32(ARM_GPIO_GPPUDCLK0, (1 << 14) | (1 << 15));
 	delay(150);
  
 	// Write 0 to GPPUDCLK0 to make it take effect.
-	mmio_write(ARM_GPIO_GPPUDCLK0, 0x00000000);
+	write32(ARM_GPIO_GPPUDCLK0, 0x00000000);
  
 	// Clear pending interrupts.
-	mmio_write(ARM_UART0_ICR, 0x7FF);
+	write32(ARM_UART0_ICR, 0x7FF);
  
 	// Set integer & fractional part of baud rate.
 	// Divider = UART_CLOCK/(16 * Baud)
@@ -31,33 +38,33 @@ void uart_init()
 	// UART_CLOCK = 3000000; Baud = 115200.
  
 	// Divider = 3000000 / (16 * 115200) = 1.627 = ~1.
-	mmio_write(ARM_UART0_IBRD, 1);
+	write32(ARM_UART0_IBRD, 1);
 	// Fractional part register = (.627 * 64) + 0.5 = 40.6 = ~40.
-	mmio_write(ARM_UART0_FBRD, 40);
+	write32(ARM_UART0_FBRD, 40);
  
 	// Enable FIFO & 8 bit data transmissio (1 stop bit, no parity).
-	mmio_write(ARM_UART0_LCRH, (1 << 4) | (1 << 5) | (1 << 6));
+	write32(ARM_UART0_LCRH, (1 << 4) | (1 << 5) | (1 << 6));
  
 	// Mask all interrupts.
-	mmio_write(ARM_UART0_IMSC, (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6) |
+	write32(ARM_UART0_IMSC, (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6) |
 	                       (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10));
  
 	// Enable UART0, receive & transfer part of UART.
-	mmio_write(ARM_UART0_CR, (1 << 0) | (1 << 8) | (1 << 9));
+	write32(ARM_UART0_CR, (1 << 0) | (1 << 8) | (1 << 9));
 }
  
 void uart_putc(unsigned char c)
 {
 	// Wait for UART to become ready to transmit.
-	while ( mmio_read(ARM_UART0_FR) & (1 << 5) ) { }
-	mmio_write(ARM_UART0_DR, c);
+	while ( read32(ARM_UART0_FR) & (1 << 5) ) { }
+	write32(ARM_UART0_DR, c);
 }
  
 unsigned char uart_getc()
 {
     // Wait for UART to have received something.
-    while ( mmio_read(ARM_UART0_FR) & (1 << 4) ) { }
-    return mmio_read(ARM_UART0_DR);
+    while ( read32(ARM_UART0_FR) & (1 << 4) ) { }
+    return read32(ARM_UART0_DR);
 }
  
 void uart_puts(const char* str)
